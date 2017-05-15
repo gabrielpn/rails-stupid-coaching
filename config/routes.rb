b433bc3c6f864e4029cd72579138868b99d2e21c
Gabriel Pimont Nogues <gpimontnogues@gmail.com>
2017-05-15 12:40:25 +0200

    Rails 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'answer', to: 'coaching#answer', as: 'answer'
+  get 'ask', to: 'coaching#ask', as: 'ask'
+  root to: 'coaching#home'
 end
+
